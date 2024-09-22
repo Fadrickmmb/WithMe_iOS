c0e88930573c491bd2233a6c11a22246fa1acb02
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-09-22 15:09:36 -0400

    Initial Commit

---
+++ WithMe_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WithMe_iOS/WithMe_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithMe_iOSApp.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct WithMe_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
