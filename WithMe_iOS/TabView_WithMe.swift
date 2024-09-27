a4043ddc17c77b31fbd2a96c4982244c13ca2a39
ericadiogo <erica.monteiro.diogo@gmail.com>
2024-09-27 13:09:30 -0400

    Added views and tabview.

---
+++ WithMe_iOS/TabView_WithMe.swift
@@ -0,0 +1,18 @@
+//
+//  TabView_WithMe.swift
+//  WithMe_iOS
+//
+//  Created by user264550 on 9/27/24.
+//
+
+import SwiftUI
+
+struct TabView_WithMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView_WithMe()
+}
---
+++ WithMe_iOS/User_ViewProfile.swift
@@ -0,0 +1,18 @@
+//
+//  User_ViewProfile.swift
+//  WithMe_iOS
+//
+//  Created by user264550 on 9/27/24.
+//
+
+import SwiftUI
+
+struct User_ViewProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    User_ViewProfile()
+}
