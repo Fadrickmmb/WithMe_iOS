5b36cf98faf1b2563e6ca795a5fc5dc1c5d555a7
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-04 11:40:46 -0400

    Working on Firebase Integration

---
+++ WithMe_iOS/Auth_LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  Auth_LoginView.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-04.
+//
+
+import SwiftUI
+
+struct Auth_LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Auth_LoginView()
+}
--- WithMe_iOS/ContentView.swift
+++ WithMe_iOS/ContentView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                
+                NavigationLink(destination: Auth_LoginView()) {
+                    Text("Login")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.top)
+
+                NavigationLink(destination: Auth_RegisterView()) {
+                    Text("Register")
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.top)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
