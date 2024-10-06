d7164fda63cbc0a87a85cf937bb0184a632d7808
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-06 00:08:53 -0400

    Auth Login

--- WithMe_iOS/ContentView.swift
+++ WithMe_iOS/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 0) {
+                
+                Image("withme_launcher")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: 530)
+                    .clipped()
+                    .edgesIgnoringSafeArea(.top)
+                
+                NavigationLink(destination: Auth_LoginView()) {
+                    Text("Login")
+                        .font(.system(size: 16, weight: .bold))
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color(red: 0.1, green: 0.18, blue: 0.19))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.top, 20)
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.4)
+                
+                NavigationLink(destination: Auth_RegisterView()) {
+                    Text("Create an Account")
+                        .font(.system(size: 16))
+                        .padding(.top, 10)
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
