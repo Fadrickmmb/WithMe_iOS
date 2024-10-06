870f750d8e8e558556df9425d95f1864e3c60887
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-06 18:49:46 -0400

    Fully Functional Admin and User Login

---
+++ WithMe_iOS/Admin_HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  Admin_HomePage.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-06.
+//
+
+import SwiftUI
+
+struct Admin_HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Admin_HomePage()
+}
