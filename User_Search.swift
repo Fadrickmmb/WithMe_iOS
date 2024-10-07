61d3483d24142138df307db77947db439712e74b
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-07 06:28:56 -0400

    Working on User Search

--- User_Search.swift
+++ User_Search.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct User_Search: View {
+    @State private var search: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Image("withme_logo")
+                    .resizable()
+                    .frame(width: 50, height: 18)
+                    .padding(20)
+                Image("withme_yummy")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding(10)
+                Image("withme_comment")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding(10)
+                Spacer()
+            }
+            HStack{
+                Text("Search")
+            }
+            HStack{
+                TextField("",text: $search)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 10)
+                    .cornerRadius(5)
+            }
+        }
     }
 }
 
