1e62bbbfd80807b2faae17483ddaacf600e31b4f
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-29 12:03:20 -0400

    Redirection to Mod is Working

---
+++ WithMe_iOS/Admin_CreateUser.swift
@@ -0,0 +1,18 @@
+//
+//  Admin_CreateUser.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-29.
+//
+
+import SwiftUI
+
+struct Admin_CreateUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Admin_CreateUser()
+}
---
+++ WithMe_iOS/Mod_HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  Mod_HomePage.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-29.
+//
+
+import SwiftUI
+
+struct Mod_HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mod_HomePage()
+}
