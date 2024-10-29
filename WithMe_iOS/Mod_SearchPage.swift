91bd9cc50553395a30600b7ba4bddd34b92a503f
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-29 12:14:34 -0400

    Mod View Profile is working

---
+++ WithMe_iOS/Mod_SearchPage.swift
@@ -0,0 +1,18 @@
+//
+//  Mod_SearchPage.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-29.
+//
+
+import SwiftUI
+
+struct Mod_SearchPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mod_SearchPage()
+}
---
+++ WithMe_iOS/Mod_ViewProfile.swift
@@ -0,0 +1,18 @@
+//
+//  Mod_ViewProfile.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-29.
+//
+
+import SwiftUI
+
+struct Mod_ViewProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mod_ViewProfile()
+}
