8a390bf2c0b3a7932dca46d843aad00fc02b52e5
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-10-29 11:06:10 -0400

    Suspension is working

---
+++ WithMe_iOS/Admin_ViewProfile.swift
@@ -0,0 +1,18 @@
+//
+//  Admin_ViewProfile.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-10-29.
+//
+
+import SwiftUI
+
+struct Admin_ViewProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Admin_ViewProfile()
+}
