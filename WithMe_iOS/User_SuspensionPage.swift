8d965f3375109376ca0fc1970ce1dda705d238cd
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-11-02 10:21:29 -0400

    Including Suspension File

---
+++ WithMe_iOS/User_SuspensionPage.swift
@@ -0,0 +1,18 @@
+//
+//  User_SuspensionPage.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-11-02.
+//
+
+import SwiftUI
+
+struct User_SuspensionPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    User_SuspensionPage()
+}
