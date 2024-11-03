309602053863a50f3d29b7fabcc0fc4ba2ebd3af
Fadrick and Erica <fadrick.barroso@gmail.com>
2024-11-02 20:28:35 -0400

    started working on admin dashboard

---
+++ WithMe_iOS/Admin_Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Admin_Dashboard.swift
+//  WithMe_iOS
+//
+//  Created by Fadrick Barroso on 2024-11-02.
+//
+
+import SwiftUI
+
+struct Admin_Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Admin_Dashboard()
+}
